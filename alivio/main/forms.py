d36a168a9e5774fcb75d9977d28d23c41919efb8
Edwin Mendoza <MENDOZA_EDWIN@student.smc.edu>
2021-07-20 16:41:21 -0700

    BE-7: Cart Page Functionality

--- alivio/main/forms.py
+++ alivio/main/forms.py
@@ -5,3 +5,10 @@ from .models import *
 class CartForm(forms.Form):
     potency= forms.ChoiceField()
     quantity = forms.IntegerField(max_value = 20 , min_value = 1)
+
+class CartQuantityItem(forms.Form):
+    potency= forms.IntegerField()
+    btn=forms.IntegerField()
+
+class DeleteButton(forms.Form):
+    potency = forms.IntegerField()
