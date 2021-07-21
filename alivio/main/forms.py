c25f3c751914a78482a639949bd19976b01fc60b
Miguel <migues3@uci.edu>
2021-07-21 14:16:39 -0700

    Updated product page to accomdate for new forms and made it responsive

--- alivio/main/forms.py
+++ alivio/main/forms.py
@@ -3,5 +3,7 @@ from django.forms import ModelForm
 from .models import *
 
 class CartForm(forms.Form):
-    potency= forms.ChoiceField()
-    quantity = forms.IntegerField(max_value = 20 , min_value = 1)
+    potency = forms.ChoiceField()
+    quantity = forms.IntegerField(max_value=20 , min_value=1, initial=1)
+    potency.widget.attrs.update({'class':'form-control mb-3'})
+    quantity.widget.attrs.update({'class': 'form-control mb-3'})
