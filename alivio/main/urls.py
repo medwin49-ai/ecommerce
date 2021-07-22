f392605ff846d1090ba161ae3ab96cfbba8a3dae
Edwin Mendoza <MENDOZA_EDWIN@student.smc.edu>
2021-07-22 15:07:30 -0700

    Implemented the front end for the checkout page with form validation

--- alivio/main/urls.py
+++ alivio/main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("" , views.index , name="index"),
-    path("products/<str:product_name>" , views.product, name="products" ),
-    path("shopping-cart", views.shopping_cart_page, name="shopping-cart"),
-    # path("add-cart" , views.add_to_cart , name = "add-to-cart"),
+    path("products/<str:product_name>" , views.product, name = "products" ),
+    path("shopping-cart", views.shopping_cart_page, name = "shopping-cart"),
+    path("checkout", views.checkout_page , name = "checkout"),
 ]
